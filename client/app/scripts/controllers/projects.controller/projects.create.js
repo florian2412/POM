f2ff68c0c553e904ee53924755a22729569c0c43
Florian Pussacq <florian2412@gmail.com>
2016-04-14 20:20:50 +0200

    Add dialog to project creation

--- client/app/scripts/controllers/projects.controller/projects.create.js
+++ client/app/scripts/controllers/projects.controller/projects.create.js
@@ -9,7 +9,7 @@
  */
 
 angular.module('pomApp')
-  .controller('ProjectsCreateCtrl', function ($scope, $state, $rootScope, databaseService) {
+  .controller('ProjectsCreateCtrl', function ($scope, $state, $rootScope, $mdDialog, databaseService) {
 
     $scope.createProject = function() {
 
@@ -25,9 +25,8 @@ angular.module('pomApp')
       var date_debut = $scope.project.startDate;
       var date_fin_theorique = $scope.project.endDate;
       /*var date_debut = new Date();
-      var date_fin_theorique = new Date();*/
+       var date_fin_theorique = new Date();*/
       //var ligne_budgetaire = $scope.budget._id;
-
 
       // TODO Faire la validation du formulaire de création de projet
       var data = "{ \"nom\": " + "\"" + nom + "\" "
@@ -43,6 +42,7 @@ angular.module('pomApp')
       databaseService.createObject('projects', data)
         .success(function (data) {
           console.log(data);
+          showSuccessDialog();
           $state.go("projects");
         })
         .error(function (err) {
@@ -56,11 +56,54 @@ angular.module('pomApp')
 
       // Infos en dur pour le moment
       $scope.budgets = ["Ligne budget 1 : 30000€", "Ligne budget 2 : 50000€", "Ligne budget 3 : 100000€"];
-
       $scope.statuts = ["Initial", "En cours", "Annulé", "Terminé"]
 
-      
-
     });
+
+    function showSuccessDialog() {
+
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Confirmation de création')
+          .textContent('Le projet ' + $scope.project.name + ' a bien été créé !')
+          .ariaLabel('Création du projet réussie')
+          .ok('Ok'));
+    };
+
+    /*
+     var success = $mdDialog.confirm()
+     .title('Confirmation de création')
+     .textContent('Le projet ' + " nom projet" + ' a bien été créé !')
+     .ariaLabel('Création d\'un projet')
+     //.targetEvent(event)
+     .ok('Oui');
+
+     $mdDialog.show(success).then(function() {
+     $state.go("projects");
+     });
+     */
+
+
+
+
+    $scope.showCancelDialog = function(event) {
+
+      var confirm = $mdDialog.confirm()
+        .title('Alerte')
+        .textContent('Etes-vous sûr d\'annuler la création du projet ?')
+        .ariaLabel('Annulation')
+        .targetEvent(event)
+        .ok('Oui')
+        .cancel('Non');
+
+      $mdDialog.show(confirm).then(function() {
+        $state.go("projects");
+      }, function() {
+
+      });
+    };
+
 
   });
