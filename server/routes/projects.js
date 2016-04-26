59fc180e4f7880d09a7315099d0fe4cc07fd3425
Tava Sang <tava.sang@gmail.com>
2016-04-26 16:44:14 +0200

    refactoring server

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,54 +2,59 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-//var mongoose = require('mongoose');
 var Project = require('../models/Project.js');
+
+router.get('/', getAllProjects);
+router.post('/', createProject);
+router.get('/:id', getProjectById);
+router.put('/:id', updateProject);
+router.delete('/:id', deleteProject);
 
 
 /* GET projects listing. */
-router.get('/', function(req, res, next) {
+function getAllProjects(req, res, next) {
     Project.find(function (err, projects) {
         if (err)
 			return next(err);
         res.json(projects);
         
    	});
-});
+};
 
 /* POST /projects */
-router.post('/', function(req, res, next) {
+function createProject(req, res, next) {
   Project.create(req.body, function (err) {
       if (err)
           return next(err);
       res.json(req.body);
   });
-});
+};
 
 /* GET /projects/id */
-router.get('/:id', function(req, res, next) {
+function getProjectById(req, res, next) {
   Project.findById(req.params.id, function (err, projects) {
     if (err) 
     	return next(err);
     res.json(projects);
   });
-});
+};
 
 /* PUT /projects/:id */
-router.put('/:id', function(req, res, next) {
+function updateProject(req, res, next) {
   Project.findByIdAndUpdate(req.params.id, req.body, function (err, projects) {
     if (err) 
     	return next(err);
     res.json(projects);
   });
-});
+};
 
 /* DELETE /projects/:id */
-router.delete('/:id', function(req, res, next) {
+function deleteProject(req, res, next) {
   Project.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) 
     	return next(err);
     res.json(post);
   });
-});
+};
 
 module.exports = router;
--- server/routes/rolesCollaborator.js
+++ server/routes/rolesCollaborator.js
@@ -2,23 +2,26 @@ var express = require('express');
 var router = express.Router();
 var RoleCollaborator = require('../models/RoleCollaborator.js');
 
+router.get('/',getAllRoles);
+router.post('/', createRole);
+
 /* GET RoleCollaborator listing. */
-router.get('/', function(req, res, next) {
+function getAllRoles(req, res, next) {
     RoleCollaborator.find(function (err, rolesCollaborator) {
         if (err)
             return next(err);
         res.json(rolesCollaborator);
 
     });
-});
+};
 
 /* POST RoleCollaborator */
-router.post('/', function(req, res, next) {
+function createRole(req, res, next) {
     RoleCollaborator.create(req.body, function (err) {
         if (err)
             return next(err);
         res.json(req.body);
     });
-});
+};
 
 module.exports = router;
