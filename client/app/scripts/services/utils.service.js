a2a701c85589ba841cce139a4d26922c1c81f957
sarrasarraf <sarra.sarraf@gmail.com>
2016-04-28 16:26:08 +0200

    Update modification task

---
+++ client/app/scripts/services/utils.service.js
@@ -0,0 +1,69 @@
+/**
+ * Created by sarra on 28/04/2016.
+ */
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pomApp.utils
+ * @description
+ * # utils
+ * Service in the pomApp for utils methods.
+ */
+angular.module('pomApp').factory('utilsService', Service);
+
+function Service($http) {
+  var service = {};
+
+  service.convertDateStringsToDates = convertDateStringsToDates;
+  service.arrayObjectIndexOf = arrayObjectIndexOf;
+  service.dateDiff = dateDiff;
+
+  return service;
+
+
+  function convertDateStringsToDates(input) {
+    var regexIso8601 = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+    
+    // Ignore things that aren't objects.
+    if (typeof input !== "object") return input;
+
+    for (var key in input) {
+      if (!input.hasOwnProperty(key)) continue;
+
+      var value = input[key];
+      var match;
+      // Check for string properties which look like dates.
+      // TODO: Improve this regex to better match ISO 8601 date strings.
+      if (typeof value === "string" && (match = value.match(regexIso8601))) {
+        // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+        var milliseconds = Date.parse(match[0]);
+        if (!isNaN(milliseconds)) {
+          input[key] = new Date(milliseconds);
+        }
+      } else if (typeof value === "object") {
+        // Recurse into object
+        convertDateStringsToDates(value);
+      }
+    }
+  };
+
+  function arrayObjectIndexOf(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+      if (myArray[i][property] === searchTerm) return i;
+    }
+    return -1;
+  };
+
+  function dateDiff(date1, date2){
+    var diff = {};                          // Initialisation du retour
+    var tmp = date2 - date1;
+
+    tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
+    diff.day = tmp;
+    console.log(diff);
+    return diff;
+  }
+
+}
+
