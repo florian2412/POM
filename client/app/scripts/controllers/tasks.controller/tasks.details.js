7504c6785262aa6cbc7dc280924897c458e07c55
Tava Sang <tava.sang@gmail.com>
2016-05-04 01:08:14 +0200

    add collaborator picker in task details

--- client/app/scripts/controllers/tasks.controller/tasks.details.js
+++ client/app/scripts/controllers/tasks.controller/tasks.details.js
@@ -10,32 +10,32 @@
 
 angular.module('pomApp').controller('TasksDetailsCtrl', TasksDetailsCtrl);
 
-function TasksDetailsCtrl($scope, $state, $stateParams, $mdDialog, databaseService,  flashService, utilsService) {
-
+function TasksDetailsCtrl($scope, $state, $rootScope, $stateParams, $mdDialog, databaseService,  flashService, utilsService) {
+    
   var vm = this;
+  var collaborateursId = [];
+  var currentProjectCollaborators = [];
 
   vm.updateTask = updateTask;
   vm.showCancelDialog = showCancelDialog;
+  vm.showCollaboratorPicker = showCollaboratorPicker;
 
   function updateTask() {
 
-    var tasksList = vm.currentProject.taches;
-    var t = tasksList[vm.currentIndexTask];
-    t.libelle = vm.task.libelle;
-    t.statut = vm.task.statut;
-    t.description = vm.task.description;
-    t.date_debut = vm.task.date_debut;
-    t.date_fin_theorique = vm.task.date_fin_theorique;
-    t.date_fin_reelle = vm.task.date_fin_reelle;
-    t.date_derniere_modif = new Date();
+    var updatedTask = vm.currentProject.taches[vm.currentIndexTask];
+    updatedTask.libelle = vm.task.libelle;
+    updatedTask.statut = vm.task.statut;
+    updatedTask.description = vm.task.description;
+    updatedTask.date_debut = vm.task.date_debut;
+    updatedTask.date_fin_theorique = vm.task.date_fin_theorique;
+    updatedTask.date_fin_reelle = vm.task.date_fin_reelle;
+    updatedTask.collaborateurs = collaborateursId;
+    updatedTask.date_derniere_modif = new Date();
 
     databaseService.updateObject('projects', $stateParams.id, vm.currentProject)
       .success(function (data) {
         flashService.success("La tâche " + vm.task.libelle + " a été mise à jour.", "", "bottom-right", true, 4);
         $state.go("projects.details.tasks");
-      })
-      .error(function (err) {
-        console.log(err);
       });
   };
 
@@ -56,27 +56,67 @@ function TasksDetailsCtrl($scope, $state, $stateParams, $mdDialog, databaseServi
     });
   };
 
+  function showCollaboratorPicker(ev) {
+
+    $mdDialog.show({
+      controller: _CollaboratorPickerController,
+      templateUrl: 'views/shared/collaborators.picker.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: false,
+      locals: {
+         collaborators: vm.collaborators 
+       },
+    });
+  };
+
+  function _CollaboratorPickerController($rootScope, $scope, $mdDialog, collaborators) {
+
+    $scope.collaborators = collaborators;
+    $scope.selection = collaborateursId;
+    $scope.hide = function() { $mdDialog.hide($scope.selection); };
+    $scope.userRole = $rootScope.userRole;
+    
+    $scope.selectCollaborator = function (collaborator) {
+      if(collaborateursId.indexOf(collaborator._id) < 0) {
+        collaborateursId.push(collaborator._id);
+        collaborator.checked = true;
+      } else {
+        collaborator.checked = false;
+        var indexCol =  collaborateursId.indexOf(collaborator._id);
+        if (indexCol > -1) { collaborateursId.splice(indexCol, 1); }
+      }
+    };  
+  }
+
   $scope.$on('$viewContentLoaded', function() {
 
     databaseService.getObjectById('projects', $stateParams.id)
       .success(function (data) {
-
-        vm.currentProject = data;
-        vm.minDateTask =  new Date(data.date_debut);
-
+        
+        var currentProjectColl = data.collaborateurs;
         var tasks = data.taches;
-
         var indexTaskToDisplay = utilsService.arrayObjectIndexOf(tasks, $stateParams.idtask, "_id");
-        vm.currentIndexTask = indexTaskToDisplay;
-
         var task = tasks[indexTaskToDisplay];
 
-        //calcul du cout
-        vm.task_cost =  utilsService.dateDiff(new Date(task.date_debut),new Date(task.date_fin_theorique));
-
-        utilsService.convertDateStringsToDates(task);
+        // Récupère les collaborateurs associés à la tâche
+        for (var i = currentProjectColl.length - 1; i >= 0; i--) {
+          databaseService.getObjectById('collaborators', currentProjectColl[i])
+            .success(function(data){
+              currentProjectCollaborators.push(data);
+            });
+        }
+        vm.collaborators = currentProjectCollaborators;
+        vm.currentProject = data;
+        vm.minDateTask =  new Date(data.date_debut); 
+        vm.currentIndexTask = indexTaskToDisplay;
         vm.task = task;
-
+        collaborateursId = task.collaborateurs;
+                
+        //calcul du cout
+        vm.task_cost = utilsService.dateDiff(new Date(task.date_debut),new Date(task.date_fin_theorique));
+        utilsService.convertDateStringsToDates(task);
       })
       .error(function (err) {
         console.log(err);
