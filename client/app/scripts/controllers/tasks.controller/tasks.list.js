c589d9044407f29e2262f4003807abe4f6217470
Tava Sang <tava.sang@gmail.com>
2016-05-09 19:09:21 +0200

    add new table for tasks list!

--- client/app/scripts/controllers/tasks.controller/tasks.list.js
+++ client/app/scripts/controllers/tasks.controller/tasks.list.js
@@ -13,22 +13,53 @@
 
 angular.module('pomApp').controller('TasksListCtrl', TasksListCtrl);
 
-function TasksListCtrl($scope, $state, databaseService, $stateParams, utilsService) {
+function TasksListCtrl($scope, $state, databaseService, $stateParams, utilsService,NgTableParams) {
 
   var vm = this;
 
   vm.showAllTasks = showAllTasks;
   vm.deleteTask = deleteTask;
-  vm.tasksDetails = tasksDetails;
+  vm.redirectTasksDetails = redirectTasksDetails;
 
-  function tasksDetails(event,id){
+  var statuts = {"initial": { "color": "blue", "class": "fa fa-plus", "statut": "Initial" },
+               "en_cours": { "color": "orange", "class": "fa fa-cog fa-spin fa-fw margin-bottom", "statut":"En cours" },
+               "termine": { "color": "green", "class": "fa fa-check-circle","statut": "Terminé(e)" },
+               "annule": { "color": "red", "class": "fa fa-times-circle", "statut": "Annulé(e)" },
+               "archive": { "color": "gray", "class": "fa fa-file-archive-o", "statut": "Archivé" }
+             };
+
+  function redirectTasksDetails(event,id){
     $state.go('projects.details.tasks.details',{"idtask":id});
   }
 
   function showAllTasks(){
-    databaseService.getObjectById('projects',$stateParams.id)
+    databaseService.getObjectById('projects', $stateParams.id)
       .success(function (data) {
         vm.tasks = data.taches;
+        for (var i = data.taches.length - 1; i >= 0; i--) {
+          switch (data.taches[i].statut)
+          {
+            case 'Initial': data.taches[i].statut = statuts.initial;
+            break;
+            case 'En cours': data.taches[i].statut = statuts.en_cours;
+            break;
+            case 'Terminé(e)': data.taches[i].statut = statuts.termine;
+            break;
+            case 'Annulé(e)': data.taches[i].statut = statuts.annule;
+            break;
+            case 'Archivé': data.taches[i].statut = statuts.archive;
+            break;
+          }
+        }
+
+        vm.tableParams = new NgTableParams({
+            page: 1, // show first page
+            count: 10 // count per page
+          }, {
+            filterDelay: 0,
+            data: data.taches
+          });
+
       })
       .error(function (err) {
         console.error(err);
@@ -46,6 +77,14 @@ function TasksListCtrl($scope, $state, databaseService, $stateParams, utilsServi
           tasks.splice(indexTaskToDelete, 1);
           vm.tasks.splice(indexTaskToDelete, 1 );
           databaseService.updateObject('projects',$stateParams.id,data).success(function (data) {});
+
+          vm.tableParams.reload().then(function(data) {
+              if (data.length === 0 && self.tableParams.total() > 0) {
+              vm.tableParams.page(self.tableParams.page() - 1);
+              vm.tableParams.reload();
+            }
+          });
+          flashService.success("Succès ! ", "La tâche a été supprimée", "bottom-right", true, 4);
         }
       })
       .error(function(err) {
