c35b2f73a5dee63d2b89928ee0a609044e26b37d
florian2412 <florian2412@gmail.com>
2016-05-09 11:37:28 +0200

    Add date début théorique to project

--- client/app/scripts/controllers/projects.controller/projects.details.js
+++ client/app/scripts/controllers/projects.controller/projects.details.js
@@ -12,7 +12,7 @@ angular.module('pomApp').controller('ProjectsDetailsCtrl', ProjectsDetailsCtrl);
 function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDialog, $state, databaseService, flashService, utilsService) {
 
   $rootScope.$on('$stateChangeStart', function() {$mdSidenav('projectSideNav').close();});
-  
+
   var collaborateursId = [];
   var vm = this;
 
@@ -26,7 +26,7 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
     databaseService.getObjectById('projects', id)
       .success(function (data) {
         var collList = data.collaborateurs;
-        
+
         for (var i = collList.length - 1; i >= 0; i--) {
           collaborateursId.push(collList[i]);
         }
@@ -46,20 +46,19 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
         var indexBudgetToSelect = utilsService.arrayObjectIndexOf(budgetsList, budgetId, "_id");
 
         vm.selectedBudget = budgetsList[indexBudgetToSelect];
-      
+
         databaseService.getObjectById('collaborators', data.chef_projet)
           .success(function (data) {
             vm.chef_projet = data.prenom + ' ' + data.nom;
           });
 
-        vm.progressProject = 100;
       })
   };
 
 
   function updateProject() {
 
-     var idProject = vm.project._id;
+    var idProject = vm.project._id;
 
     // TODO SAVE BUDGETS
     var ligne_budgetaire_id = vm.selectedBudget._id;
@@ -68,7 +67,12 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
       "montant_restant": vm.selectedBudget.montant
     };
 
-    var data = {
+    var data;
+
+    // TODO Changer le status de 'Initial' à 'En cours' lorsqu'on affecte une date de début réelle
+
+
+    data = {
       "nom" : vm.project.nom,
       "statut" : vm.project.statut,
       "date_debut" : vm.project.date_debut,
@@ -76,9 +80,13 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
       "date_fin_reelle" : vm.project.date_fin_reelle,
       "ligne_budgetaire" : ligne_budgetaire,
       "date_derniere_modif" : new Date(),
-      "collaborateurs": collaborateursId
+      "collaborateurs": collaborateursId,
+      "date_debut_reelle" : vm.project.date_debut_reelle
     };
-  
+
+
+
+
     databaseService.updateObject('projects', idProject, data)
       .success(function (data) {
         flashService.success("Le projet " + vm.project.nom + " a bien été mis à jour !", "", "bottom-right", true, 4);
@@ -109,20 +117,19 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
     databaseService.getAllObjects('budgets').success(function (data) { vm.budgets = data.data; });
 
     databaseService.getAllObjects('collaborators').success(function (data) { vm.collaborators = data;});
-  
-    databaseService.getSettings('statuts').success(function(data){ 
+
+    databaseService.getSettings('statuts').success(function(data){
       data.splice(data.indexOf("Archivé"),1);
       data.splice(data.indexOf("Initial"),1);
-      vm.statuts = data; 
+      vm.statuts = data;
     });
-  
+
     vm.getProjectById($stateParams.id);
-    
+
   });
 
   function showCollaboratorPicker(ev) {
-
-      $mdDialog.show({
+    $mdDialog.show({
         controller: _CollaboratorPickerController,
         templateUrl: 'views/shared/collaborators.picker.html',
         parent: angular.element(document.body),
@@ -130,14 +137,15 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
         clickOutsideToClose:true,
         fullscreen: false,
         locals: {
-           collaborators: vm.collaborators 
-         },
+          collaborators: vm.collaborators
+        }
       })
       .then(function(count) {
         console.log(count);
         //vm.numberOfCollaborators = count.length;
       });
-   };
+  };
+
 
   function _CollaboratorPickerController($rootScope, $scope, $mdDialog, collaborators) {
 
@@ -156,6 +164,6 @@ function ProjectsDetailsCtrl($rootScope, $scope, $stateParams, $mdSidenav, $mdDi
         if (indexCol > -1) { collaborateursId.splice(indexCol, 1); }
       }
     };
-    
+
   }
 };
