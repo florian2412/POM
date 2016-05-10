ec49e65f50cf2e49f7a03303fbd35a3791dd7596
florian2412 <florian2412@gmail.com>
2016-05-10 15:43:20 +0200

    Add new graph : columnRangeChart

--- client/app/scripts/controllers/statistics.js
+++ client/app/scripts/controllers/statistics.js
@@ -27,6 +27,67 @@ angular.module('pomApp')
       buildTasksDurationBarChart(project._id);
       //buildTasksDurationColumnRangeChart(project._id);
 
+
+
+
+      $(function () {
+        $(document).ready(function () {
+          $('#columnrangechart-project').highcharts({
+            chart: {
+              type: 'columnrange',
+              inverted: true
+            },
+            title: {
+              text: 'Temperature variation by month'
+            },
+            subtitle: {
+              text: 'Observed in Vik i Sogn, Norway'
+            },
+            xAxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+              title: {
+                text: 'Temperature ( °C )'
+              }
+            },
+            tooltip: {
+              valueSuffix: '°C'
+            },
+            plotOptions: {
+              columnrange: {
+                dataLabels: {
+                  enabled: true,
+                  formatter: function () {
+                    return this.y + '°C';
+                  }
+                }
+              }
+            },
+            legend: {
+              enabled: false
+            },
+            series: [{
+              name: 'Temperatures',
+              data: [
+                [-9.7, 9.4],
+                [-8.7, 6.5],
+                [-3.5, 9.4],
+                [-1.4, 19.9],
+                [0.0, 22.6],
+                [2.9, 29.5],
+                [9.2, 30.7],
+                [7.3, 26.5],
+                [4.4, 18.0],
+                [-3.1, 11.4],
+                [-5.2, 10.4],
+                [-13.5, 9.8]
+              ]
+            }]
+          });
+        });
+      });
+
     }
 
     /**********************************************************************************************************
@@ -133,13 +194,15 @@ angular.module('pomApp')
         }
       }
 
+      var idChart = 'barchart-project';
+      var titleChart = 'Comparaison entre la durée théorique et réelle de chaque tâche du projet';
+      var pointFormatChart = 'Coût :<b>{point.y} €</b> <br> Soit :<b>{point.percentage:.1f}%</b>';
+
       if(tasksName.length != 0) {
-        var idChart = 'barchart-project';
-        var titleChart = 'Comparaison entre la durée théorique et réelle de chaque tâche du projet';
-        var pointFormatChart = 'Coût :<b>{point.y} €</b> <br> Soit :<b>{point.percentage:.1f}%</b>';
+
         chartsService.buildBarChartTasksDuration(idChart, titleChart, pointFormatChart, tasksName, tasksTheoricDuration, tasksRealDuration);
       } else {
-
+        chartsService.buildBarChartTasksDuration(idChart, titleChart, pointFormatChart, tasksName, tasksTheoricDuration, tasksRealDuration);
       }
     }
 
