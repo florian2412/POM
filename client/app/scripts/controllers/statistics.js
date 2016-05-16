1e06cf0884c7868dc88e3617aef4ca2469b8565f
florian2412 <florian2412@gmail.com>
2016-05-16 15:40:26 +0200

    Fix bug

--- client/app/scripts/controllers/statistics.js
+++ client/app/scripts/controllers/statistics.js
@@ -23,7 +23,6 @@ angular.module('pomApp')
       buildProjectsByStatusBarChart();
       buildProjectsDurationBarChart();
       buildBarChartTasksByProjects();
-
     }
 
     function updateChartProject() {
