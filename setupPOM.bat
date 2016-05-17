9c909b8b631494ef7c05604b8bef10c240d8cfc4
florian2412 <florian2412@gmail.com>
2016-05-17 16:21:17 +0200

    Update and fix setup scripts

---
+++ setupPOM.bat
@@ -0,0 +1,4 @@
+echo '## Installation du serveur de POM ##'
+
+echo '## Installation du serveur REST en local sur le port 3000 ##'
+cd server/ && npm install&
--- setupServer.bat
+++ setupServer.bat
@@ -1,7 +1,4 @@
 echo '## Installation de POM ##'
 
-echo '## Installation du serveur REST en local sur le port 3000 ##'
-cd server/ && npm install&
-
 echo '## Installation du client POM en local sur le port 9000 ##'
 cd client/ && npm install& && bower install&
