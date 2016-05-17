072f6edb9e22d5f3954375e6a35846e93fc1dcce
florian2412 <florian2412@gmail.com>
2016-05-17 15:42:28 +0200

    Update database test

--- loadDataset.bat
+++ loadDataset.bat
@@ -3,4 +3,4 @@ echo '##mongodump -d databaseName -o databaseBackup##'
 echo '## Chargement du jeu de données ##'
 set here=%cd%
 echo %here%
-cd C:\Program Files\MongoDB\Server\3.2\bin && .\mongorestore.exe -d dbPOM_test %here%\data\dbPOM &
+cd C:\Program Files\MongoDB\Server\3.2\bin && .\mongorestore.exe -d dbPOM %here%\data\dbPOMbackup\dbPOM &
